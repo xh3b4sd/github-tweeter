f69846be4fc2cc7753e24e39eda3831a87c2cfb2
Tim Schindler <xh3b4sd@gmail.com>
2019-08-11 14:21:45 +0200

    add initial poc (#1)

--- README.md
+++ README.md
@@ -1,1 +1,35 @@
 # github-tweeter
+
+
+
+```
+export GITHUB_TWEETER_GITHUB_TOKEN=$(cat ~/.credential/github-tweeter-github-token)
+export TWITTER_CONSUMER_KEY=$(cat ~/.credential/twitter-consumer-key)
+export TWITTER_CONSUMER_SECRET=$(cat ~/.credential/twitter-consumer-secret)
+export TWITTER_ACCESS_TOKEN=$(cat ~/.credential/twitter-access-token)
+export TWITTER_ACCESS_SECRET=$(cat ~/.credential/twitter-access-secret)
+```
+
+
+
+```
+$ go run main.go
+{"caller":"github-tweeter/main.go:79","level":"debug","message":"initializing github client","time":"2019-08-11T12:19:09.423082+00:00"}
+{"caller":"github-tweeter/main.go:87","level":"debug","message":"initialized github client","time":"2019-08-11T12:19:09.423237+00:00"}
+{"caller":"github-tweeter/main.go:99","level":"debug","message":"initializing twitter client","time":"2019-08-11T12:19:09.423274+00:00"}
+{"caller":"github-tweeter/main.go:106","level":"debug","message":"initialized twitter client","time":"2019-08-11T12:19:09.423342+00:00"}
+{"caller":"github-tweeter/main.go:143","level":"debug","message":"finding latest commit","time":"2019-08-11T12:19:09.423354+00:00"}
+{"caller":"github-tweeter/main.go:159","level":"debug","message":"found latest commit `6857e548e89f4f91c51473645295d2e1f7b92f2f`","time":"2019-08-11T12:19:09.855181+00:00"}
+{"caller":"github-tweeter/main.go:169","level":"debug","message":"finding latest file","time":"2019-08-11T12:19:09.855234+00:00"}
+{"caller":"github-tweeter/main.go:178","level":"debug","message":"found latest file `philosophy/0000422`","time":"2019-08-11T12:19:10.166257+00:00"}
+{"caller":"github-tweeter/main.go:190","level":"debug","message":"computing total number of files","time":"2019-08-11T12:19:10.166299+00:00"}
+{"caller":"github-tweeter/main.go:199","level":"debug","message":"computed total number of files 422","time":"2019-08-11T12:19:10.166346+00:00"}
+{"caller":"github-tweeter/main.go:210","level":"debug","message":"choosing random file","time":"2019-08-11T12:19:10.166369+00:00"}
+{"caller":"github-tweeter/main.go:226","level":"debug","message":"chose random file `philosophy/0000146`","time":"2019-08-11T12:19:10.166453+00:00"}
+{"caller":"github-tweeter/main.go:231","level":"debug","message":"finding content","time":"2019-08-11T12:19:10.166486+00:00"}
+{"caller":"github-tweeter/main.go:247","level":"debug","message":"found content `The #nice shit is just #around #the #corner. You just need to #pick #it #up and #make #candy out of it.`","time":"2019-08-11T12:19:10.376245+00:00"}
+{"caller":"github-tweeter/main.go:257","level":"debug","message":"verifying twitter credentials for user","time":"2019-08-11T12:19:10.376297+00:00"}
+{"caller":"github-tweeter/main.go:269","level":"debug","message":"verified twitter credentials for user `xh3b4sd`","time":"2019-08-11T12:19:10.626737+00:00"}
+{"caller":"github-tweeter/main.go:280","level":"debug","message":"tweeting content","time":"2019-08-11T12:19:10.6268+00:00"}
+{"caller":"github-tweeter/main.go:287","level":"debug","message":"tweeted content","time":"2019-08-11T12:19:10.826546+00:00"}
+```
---
+++ main.go
@@ -0,0 +1,291 @@
+package main
+
+import (
+	"context"
+	"crypto/rand"
+	"fmt"
+	"os"
+	"regexp"
+	"strconv"
+	"strings"
+	"time"
+
+	"github.com/dghubble/go-twitter/twitter"
+	"github.com/dghubble/oauth1"
+	"github.com/giantswarm/backoff"
+	"github.com/giantswarm/microerror"
+	"github.com/giantswarm/micrologger"
+	"github.com/google/go-github/github"
+	"github.com/the-anna-project/random"
+	"golang.org/x/oauth2"
+)
+
+const (
+	org  = "xh3b4sd"
+	repo = "content"
+	dir  = "philosophy"
+)
+
+var (
+	githubToken = os.Getenv("GITHUB_TWEETER_GITHUB_TOKEN")
+
+	twitterConsumerKey    = os.Getenv("TWITTER_CONSUMER_KEY")
+	twitterConsumerSecret = os.Getenv("TWITTER_CONSUMER_SECRET")
+	twitterAccessToken    = os.Getenv("TWITTER_ACCESS_TOKEN")
+	twitterAccessSecret   = os.Getenv("TWITTER_ACCESS_SECRET")
+)
+
+var (
+	// seqExp is the regular expression for the conventional sequence number
+	// encoded in the content file names. File names must follow a pattern like
+	// shown below.
+	//
+	//     philosophy/0000001
+	//     philosophy/0000002
+	//     ...
+	//     philosophy/0000054
+	//     philosophy/0000055
+	//
+	seqExp = regexp.MustCompile(`([0-9]+)$`)
+	// wspExp is the regular expression used to trim space and line control
+	// characters from the gathered content. Content might be wrapped or formatted
+	// in a certain way which makes it necessary to trim spaces in order to cary
+	// out sanitized content for a Tweet on Twitter.
+	wspExp = regexp.MustCompile(`[\n\r\t]`)
+)
+
+func main() {
+	err := mainE(context.Background())
+	if err != nil {
+		panic(microerror.Stack(err))
+	}
+}
+
+func mainE(ctx context.Context) error {
+	var err error
+
+	var newLogger micrologger.Logger
+	{
+		c := micrologger.Config{}
+
+		newLogger, err = micrologger.New(c)
+		if err != nil {
+			return microerror.Mask(err)
+		}
+	}
+
+	var ghClient *github.Client
+	{
+		newLogger.LogCtx(ctx, "level", "debug", "message", "initializing github client")
+
+		c := oauth2.NewClient(ctx, oauth2.StaticTokenSource(
+			&oauth2.Token{AccessToken: githubToken},
+		))
+
+		ghClient = github.NewClient(c)
+
+		newLogger.LogCtx(ctx, "level", "debug", "message", "initialized github client")
+	}
+
+	// Create a Twitter API client for further use below. The required credentials
+	// are generated via a Twitter app that has to be set up properly. Therefore
+	// you need to register an application account and create an app in the apps
+	// dashboard.
+	//
+	//     https://developer.twitter.com/en/apps
+	//
+	var twClient *twitter.Client
+	{
+		newLogger.LogCtx(ctx, "level", "debug", "message", "initializing twitter client")
+
+		config := oauth1.NewConfig(twitterConsumerKey, twitterConsumerSecret)
+		token := oauth1.NewToken(twitterAccessToken, twitterAccessSecret)
+
+		twClient = twitter.NewClient(config.Client(oauth1.NoContext, token))
+
+		newLogger.LogCtx(ctx, "level", "debug", "message", "initialized twitter client")
+	}
+
+	var newRandom random.Service
+	{
+		c := random.ServiceConfig{
+			BackoffFactory: func() random.Backoff {
+				return backoff.NewMaxRetries(3, time.Second)
+			},
+			RandFactory: rand.Int,
+
+			RandReader: rand.Reader,
+			Timeout:    1 * time.Second,
+		}
+
+		newRandom, err = random.NewService(c)
+		if err != nil {
+			return microerror.Mask(err)
+		}
+	}
+
+	// Fetch the latest commit made in the configured folder. The sha is used to
+	// get its associated file which changed with the commit. The name of this
+	// file then by convention indicates the highest number of files in the
+	// sequence of existing file names. The example file names below describe the
+	// convention of numbers defining the file sequence. The method below fetches
+	// the sha that assumedly added file055 which tells us that there are 55 files
+	// to chose from randomly.
+	//
+	//     path/file001
+	//     path/file002
+	//     ...
+	//     path/file054
+	//     path/file055
+	//
+	var sha string
+	{
+		newLogger.LogCtx(ctx, "level", "debug", "message", "finding latest commit")
+
+		in := &github.CommitsListOptions{
+			Path: dir,
+			ListOptions: github.ListOptions{
+				PerPage: 1,
+			},
+		}
+
+		out, _, err := ghClient.Repositories.ListCommits(ctx, org, repo, in)
+		if err != nil {
+			return microerror.Mask(err)
+		}
+
+		sha = out[0].GetSHA()
+
+		newLogger.LogCtx(ctx, "level", "debug", "message", fmt.Sprintf("found latest commit %#q", sha))
+	}
+
+	// We fetch the file name using the commit hash found above. The commit is
+	// expected to have changed exactly one file.
+	//
+	//     path/file055
+	//
+	var file string
+	{
+		newLogger.LogCtx(ctx, "level", "debug", "message", "finding latest file")
+
+		out, _, err := ghClient.Repositories.GetCommit(ctx, org, repo, sha)
+		if err != nil {
+			return microerror.Mask(err)
+		}
+
+		file = out.Files[0].GetFilename()
+
+		newLogger.LogCtx(ctx, "level", "debug", "message", fmt.Sprintf("found latest file %#q", file))
+	}
+
+	// We lookup the number of files in the traversed folder. The name of the
+	// changed file is expected to comply with a format as such that the end of
+	// the file name is a number of a sequence of files. When we extract the
+	// number of the example file name above it should result in the following.
+	//
+	//     55
+	//
+	var number int
+	{
+		newLogger.LogCtx(ctx, "level", "debug", "message", "computing total number of files")
+
+		m := seqExp.FindString(file)
+
+		number, err = strconv.Atoi(m)
+		if err != nil {
+			return microerror.Mask(err)
+		}
+
+		newLogger.LogCtx(ctx, "level", "debug", "message", fmt.Sprintf("computed total number of files %d", number))
+	}
+
+	// We compute a random file of which we take the content to tweet from. Since
+	// we have the number of the upper end of the file sequence we generate a
+	// random number and put together the new file name including eventual
+	// padding. Padding is necessary because the upper number might have 4 digits
+	// while the chosen number might have 2, which implies to add a padding of 2
+	// padding characters assuming a consistent conventional file name format.
+	var path string
+	{
+		newLogger.LogCtx(ctx, "level", "debug", "message", "choosing random file")
+
+		i, err := newRandom.CreateMax(number + 1)
+		if err != nil {
+			return microerror.Mask(err)
+		}
+		n := strconv.Itoa(i)
+		p := len(file) - len(seqExp.ReplaceAllString(file, "")) - len(n)
+
+		path += dir
+		path += "/"
+		for i := 0; i < p; i++ {
+			path += "0"
+		}
+		path += n
+
+		newLogger.LogCtx(ctx, "level", "debug", "message", fmt.Sprintf("chose random file %#q", path))
+	}
+
+	var content string
+	{
+		newLogger.LogCtx(ctx, "level", "debug", "message", "finding content")
+
+		in := &github.RepositoryContentGetOptions{}
+
+		out, _, _, err := ghClient.Repositories.GetContents(ctx, org, repo, path, in)
+		if err != nil {
+			return microerror.Mask(err)
+		}
+
+		c, err := out.GetContent()
+		if err != nil {
+			return microerror.Mask(err)
+		}
+
+		content = strings.TrimSpace(wspExp.ReplaceAllString(c, " "))
+
+		newLogger.LogCtx(ctx, "level", "debug", "message", fmt.Sprintf("found content %#q", content))
+	}
+
+	// We just make sure that we deal with valid credentials and collect
+	// information about the user on which behalf we are going to tweet below.
+	//
+	//     https://developer.twitter.com/en/docs/accounts-and-users/manage-account-settings/api-reference/get-account-verify_credentials
+	//
+	var userName string
+	{
+		newLogger.LogCtx(ctx, "level", "debug", "message", "verifying twitter credentials for user")
+
+		p := &twitter.AccountVerifyParams{
+			SkipStatus: twitter.Bool(true),
+		}
+		user, _, err := twClient.Accounts.VerifyCredentials(p)
+		if err != nil {
+			return microerror.Mask(err)
+		}
+
+		userName = user.ScreenName
+
+		newLogger.LogCtx(ctx, "level", "debug", "message", fmt.Sprintf("verified twitter credentials for user %#q", userName))
+	}
+
+	// Once the necessary content is gathered it can be tweeted using the Twitter
+	// client initialized above. For more information about the API client and API
+	// specs see the following resources.
+	//
+	//     https://godoc.org/github.com/dghubble/go-twitter/twitter#StatusService.Update
+	//     https://developer.twitter.com/en/docs/tweets/post-and-engage/api-reference/post-statuses-update
+	//
+	{
+		newLogger.LogCtx(ctx, "level", "debug", "message", "tweeting content")
+
+		_, _, err := twClient.Statuses.Update(content, nil)
+		if err != nil {
+			return microerror.Mask(err)
+		}
+
+		newLogger.LogCtx(ctx, "level", "debug", "message", "tweeted content")
+	}
+
+	return nil
+}
